91fee347555dc0446d0612351fe9d957daaa1fd1
EfeDurmaz16 <efebarandurmaz05@gmail.com>
2025-11-30 18:46:14 +0300

    Add multi-token wallet support (USDC, USDT, PYUSD, EURC)

--- sardis_core/models/__init__.py
+++ sardis_core/models/__init__.py
@@ -1,7 +1,7 @@
 """Data models for Sardis Core."""
 
 from .agent import Agent
-from .wallet import Wallet
+from .wallet import Wallet, TokenType, TokenBalance, TOKEN_INFO
 from .transaction import Transaction, TransactionStatus
 from .virtual_card import VirtualCard
 from .merchant import Merchant
@@ -9,9 +9,11 @@ from .merchant import Merchant
 __all__ = [
     "Agent",
     "Wallet",
+    "TokenType",
+    "TokenBalance",
+    "TOKEN_INFO",
     "Transaction",
     "TransactionStatus",
     "VirtualCard",
     "Merchant",
 ]
-
